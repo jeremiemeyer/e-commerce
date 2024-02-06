ea41ec2410d474050181061d5af5ef339baa7627
Jeremie Meyer <jeremie.meyer357@gmail.com>
2024-02-06 18:29:02 +0100

    update

--- e-commerce-store/app/(routes)/page.tsx
+++ e-commerce-store/app/(routes)/page.tsx
@@ -1,7 +1,8 @@
 import getBillboard from "@/actions/get-billboard"
+import getProducts from "@/actions/get-products"
 import Container from "@/components/ui/container"
 import Billboard from "@/components/billboard"
-import getProducts from "@/actions/get-products"
+import ProductList from "@/components/product-list"
 
 export const revalidate = 0
 
@@ -13,6 +14,9 @@ const HomePage = async () => {
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
   )
