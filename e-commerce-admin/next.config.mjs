2afccd5fbb70deb1502c01635ea94b5ffad7ae97
Jeremie Meyer <jeremie.meyer357@gmail.com>
2024-02-26 15:56:34 +0100

    optimizefonts false

--- e-commerce-admin/next.config.mjs
+++ e-commerce-admin/next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "res.cloudinary.com",
-                port: '',
-                pathname: '/**',
-            }
-        ]
-    }
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  optimizeFonts: false, // weird font without this line for some reason
+}
 
-export default nextConfig;
+export default nextConfig
