0b697ebc20d008bd5d8ba4047839018df316129e
Jeremie Meyer <jeremie.meyer357@gmail.com>
2024-02-26 14:25:20 +0100

    added cart to product page + fixed bugs

--- e-commerce-store/components/footer.tsx
+++ e-commerce-store/components/footer.tsx
@@ -3,11 +3,11 @@ const Footer = () => {
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2024 MyStore. All Rights Reserved.
+                    &copy; 2024 AwesomeStore. This is a demo store with no commercial purposes.
                 </p>
             </div>
         </footer>
     )
-}
+} 
 
 export default Footer
--- e-commerce-store/components/info.tsx
+++ e-commerce-store/components/info.tsx
@@ -1,16 +1,26 @@
 "use client"
 
 import { ShoppingCart } from "lucide-react"
+import { MouseEventHandler } from "react"
 
 import { Product } from "@/types"
 import Currency from "@/components/ui/currency"
 import Button from "@/components/ui/button"
+import IconButton from "@/components/ui/icon-button"
+import useCart from "@/hooks/use-cart"
 
 interface InfoProps {
   data: Product
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart()
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation()
+    cart.addItem(data)
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -34,7 +44,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3 text-white">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart />
         </Button>
